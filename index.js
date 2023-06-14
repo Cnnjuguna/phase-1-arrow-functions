17860065268ea6dc68e48713cdadea4af110ebff
njugudev <njugudev@pop-os.localdomain>
2023-06-14 18:10:11 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = ( ) => {
+    return 2000 / 100
+}
+
+console.log(divide(2000, 100))
+
+const square = (x) => {
+    return x * x
+}
+
+console.log(square(26))
+
+const add = (a, b) => {
+    return  a + b 
+}
+
+console.log(add(7, 6))
+
